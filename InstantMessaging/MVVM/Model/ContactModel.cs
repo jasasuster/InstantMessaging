e1e378c14a048ea3dd9750826cd638eb0c8da840
jasasuster <92645266+jasasuster@users.noreply.github.com>
2023-03-09 21:20:19 +0100

    Add project files.

---
+++ InstantMessaging/MVVM/Model/ContactModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InstantMessaging.MVVM.Model
+{
+    class ContactModel
+    {
+        public ContactModel(string username, string imageSource) { 
+            Username = username;
+            ImageSource = imageSource;
+            Messages = new ObservableCollection<MessageModel>();
+        }
+
+        private string Username { get; set; }
+        private string ImageSource { get; set; }
+        private ObservableCollection<MessageModel> Messages { get; set; }
+
+
+
+    }
+}
---
+++ InstantMessaging/MVVM/Model/MessageModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InstantMessaging.MVVM.Model
+{
+    class MessageModel
+    {
+        private string Username { get; set; }
+        private string UsernameColor { get; set; }
+        private string ImageSource { get; set; }
+        private string Message { get; set; }
+        private DateTime Time { get; set; }
+        private bool IsMine { get; set; }
+        private bool? FirstMessage { get; set; }
+    }
+}
